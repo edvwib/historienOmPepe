b66e0713db97e4c1f60c7d3982eb5b4dbf8beb89
Edvin Wiberg <edde275@gmail.com>
2017-10-06 22:07:08 +0200

    add scroll with mouse and keyboard to next image

--- static/js/interactive.js
+++ static/js/interactive.js
@@ -1,3 +1,59 @@
 $(document).ready(function(){
   //alert("Document ready!");
+
+  var i = 0; //User for keeping track of user position on page
+  //Scrolling with keyboard
+  $(function () {
+    $(document).keydown(function (evt) {
+      var k = evt.keyCode;
+      evt.preventDefault();
+      if(k === 40 || k === 83 || k === 34 || k === 32){ //Down arrow key, s, pagedown or spacebar
+        scrollDown();
+      }
+      if(k === 38 || k === 87 || k === 33){ //Up arrow key, w or pageup
+        scrollUp();
+      }
+    });
+  });
+
+
+
+  //Scroll with mousewheel
+  $(window).bind('mousewheel', function(event) {
+    event.preventDefault();
+    if (event.originalEvent.wheelDelta >= 0) { //Scroll up
+      console.log('Scroll up');
+      scrollUp();
+    }
+    else { //Scroll down
+      console.log('Scroll down');
+      scrollDown();
+    }
+  });
+
+
+
+
+
+
+
+
+
+  function scrollUp(){
+    i--;
+    if (i < 0) {
+      i = 0;
+    }else {
+      $('html,body').animate({ scrollTop:$('#page' + i).offset().top}, 2000);
+    }
+  }
+  function scrollDown(){
+    i++;
+    if (i > 13) {
+      i = 13;
+    }else {
+      $('html,body').animate({ scrollTop:$('#page' + i).offset().top}, 2000);
+    }
+  }
+
 });
