002dda4aea5d311dedd530a7c1bd107a96e76c9f
Edvin Wiberg <edde275@gmail.com>
2017-10-10 23:38:52 +0200

    add page location to url if scrolled
    also renamed some variables for easier reading and removed logging

--- static/js/interactive.js
+++ static/js/interactive.js
@@ -2,9 +2,27 @@ $(document).ready(function(){
   //alert("Document ready!");
   var d = new Date();
   var time = 0;
+  oldURLLoc = window.location.href.split("#page").pop();
+  baseURL = window.location.href.substring(0, window.location.href.indexOf( "#" ));
+  var currentPage = null;
+  if (oldURLLoc.charAt(oldURLLoc.length-1) == 'l') {
+    currentPage = -1;
+    scrollDown(0);
+  }else if(currentPage === 0 || currentPage === 13) {//Do not scroll to active page if user is at start/end
 
-  $(this).scrollTop(0); //Scrolls to top of page on refresh
-  var i = 0; //User for keeping track of user position on page
+  }else {
+    currentPage = oldURLLoc; //Used for keeping track of user position on page
+    currentPage--;
+    scrollDown(0);
+  }
+
+
+  $(window).bind('hashchange', function() {//Update currentPage if user manually change URL
+     oldURLLoc = window.location.href.split("#page").pop();
+     currentPage = oldURLLoc;
+   });
+
+
   //Scrolling with keyboard
   $(function () {
     $(document).keydown(function (evt) {
@@ -13,11 +31,11 @@ $(document).ready(function(){
       if (d.getTime() >= time+2000) {
         if(k === 40 || k === 83 || k === 34 || k === 32){ //Down arrow key, s, pagedown or spacebar
           evt.preventDefault();
-          scrollDown();
+          scrollDown(2000);
         }
         if(k === 38 || k === 87 || k === 33){ //Up arrow key, w or pageup
           evt.preventDefault();
-          scrollUp();
+          scrollUp(2000);
         }
         time = d.getTime();
       }
@@ -32,11 +50,10 @@ $(document).ready(function(){
     var delta = event.originalEvent.wheelDelta;
     d = new Date();
     if (d.getTime() >= time+2000) {
-      console.log("ifstart");
       if (delta > 0) { //Scroll up
-        scrollUp();
+        scrollUp(2000);
       }else if(delta < 0){ //Scroll down
-        scrollDown();
+        scrollDown(2000);
       }
       time = d.getTime();
     }
@@ -50,22 +67,29 @@ $(document).ready(function(){
 
 
 
-  function scrollUp(){
-    console.log(i);
-    i--; //Update current location
-    if (i < 0) { //If first page = do no scroll up
-      i = 0;
+  function scrollUp(speed){
+    currentPage--; //Update current location
+    if (currentPage < 0) { //If first page = do no scroll up
+      currentPage = 0;
     }else {
-      $('html,body').animate({ scrollTop:$('#page' + i).offset().top}, 2000);
+      $('html,body').animate({ scrollTop:$('#page' + currentPage).offset().top}, speed);
     }
+    updateURL();
   }
-  function scrollDown(){
-    console.log(i);
-    i++; //Update current location
-    if (i > 13) { //If last page = do no scroll down
-      i = 13;
+  function scrollDown(speed){
+    currentPage++; //Update current location
+    if (currentPage > 13) { //If last page = do no scroll down
+      currentPage = 13;
     }else {
-      $('html,body').animate({ scrollTop:$('#page' + i).offset().top}, 2000);
+      $('html,body').animate({ scrollTop:$('#page' + currentPage).offset().top}, speed);
     }
+    updateURL();
   }
+
+  function updateURL(){
+    window.location.href = baseURL + "#page" + currentPage;
+  }
+
+
+
 });
